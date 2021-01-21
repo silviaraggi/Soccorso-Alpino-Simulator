b54fdf633ce19c9e50754c9221668bb8045a9362
andreabonapoli <s277925@studenti.polito.it>
2021-01-21 12:24:30 +0100

    Creato terreno ed alberi

---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int width = 512;
+    public int height = 512;
+    public int depth = 10;
+    public float scale = 3f;
+
+    private void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain (TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);    //PERLIN NOSE VALUE
+            }
+        }
+
+        return heights;
+    }
+        float CalculateHeight (int x, int y)
+        {
+            float xCoord = (float)x / width * scale;
+            float yCoord = (float)y / height * scale;
+
+            return Mathf.PerlinNoise(xCoord, yCoord);
+        }
+}
