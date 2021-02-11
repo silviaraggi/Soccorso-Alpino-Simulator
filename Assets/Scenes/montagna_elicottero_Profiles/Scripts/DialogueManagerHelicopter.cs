ac51097bd830eae32d9108acafe921e7824284f8
DESKTOP-58HGMR5\Rosario1 <milazzorosario@hotmail.it>
2021-02-11 21:55:22 +0100

    Elicottero

---
+++ Assets/Scenes/montagna_elicottero_Profiles/Scripts/DialogueManagerHelicopter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManagerHelicopter : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public bool dialogue_bool;
+    public Animator animator;
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        dialogue_bool = false;
+
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonCharacterControllerHelicopter>().SetLocked(true);
+        dialogue_bool = true;
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+
+
+    void EndDialogue()
+    {
+        Debug.Log("fine");
+        animator.SetBool("IsOpen", false);
+        dialogue_bool = false;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FPSInteractionManagerHelicopter>().SetUnlocked(true);
+
+    }
+
+}
---
+++ Assets/Scenes/montagna_elicottero_Profiles/Scripts/InteractablePersonHelicopter.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePersonHelicopter : Interactable
+{
+    public bool animatable; //dialogable
+    public bool collectable; //follow
+    public bool interact = false; //dialog
+    public bool collect = false; //follow
+    public DialogueTriggerHelicopter dialoguetrigger;
+    public bool dialogue = false;
+    Material[] mat;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        if (GetComponent<SC_NPCFollow>())
+            collectable = true;
+        if (gameObject.GetComponent<Renderer>())
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+            mat = GetComponent<Renderer>().materials;
+        }
+        if (gameObject.GetComponent<Collider>())
+            gameObject.GetComponent<Collider>().enabled = true;
+
+    }
+
+
+    public void Update()
+    {
+        if (this.animatable)
+        {
+            dialogue = GameObject.Find("DialogueManager").GetComponent<DialogueManagerHelicopter>().dialogue_bool;
+            interact = dialogue;
+        }
+    }
+
+    public override void GlowUp(GameObject changeColor)
+    {
+        if (!interact)
+        {
+            {
+                /* if (mat != null)
+                 {
+                     for (int i = 0; i < mat.Length; i++)
+                     {
+                         mat[i].EnableKeyword("_EMISSION");
+                         mat[i].SetColor("_EmissionColor", new Vector4(0.15f, 0.30f, 0.30f, 0));
+                     }
+                 }*/
+                //else
+                {
+                    /*if (changeColor.transform.GetComponent<Renderer>())
+                    {
+                        mat = changeColor.transform.GetComponent<Renderer>().materials;
+                        if (mat != null)
+                        {
+                            for (int i = 0; i < mat.Length; i++)
+                            {
+                                mat[i].EnableKeyword("_EMISSION");
+                                mat[i].SetColor("_EmissionColor", new Vector4(0.30f, 0.30f, 0.30f, 0));
+                            }
+                        }
+                    }*/
+                    Transform[] allChildren = GetComponentsInChildren<Transform>();
+                    foreach (Transform child in allChildren)
+                    {
+                        if (child.gameObject.GetComponent<Renderer>())
+                        {
+                            if (child.gameObject.GetComponent<Renderer>().materials.Length > 0)
+
+                                for (int i = 0; i < child.gameObject.GetComponent<Renderer>().materials.Length; i++)
+                                {
+                                    child.gameObject.GetComponent<Renderer>().materials[i].EnableKeyword("_EMISSION");
+                                    child.gameObject.GetComponent<Renderer>().materials[i].SetColor("_EmissionColor", new Vector4(0.15f, 0.30f, 0.30f, 0));
+                                }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public override void TurnOff()
+    {
+        /*if (mat != null)
+            for (int i = 0; i < mat.Length; i++)
+            {
+                mat[i].DisableKeyword("_EMISSION");
+            }
+        else
+        {*/
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            if (child.gameObject.GetComponent<Renderer>())
+            {
+                if (child.gameObject.GetComponent<Renderer>().materials.Length > 0)
+
+                    for (int i = 0; i < child.gameObject.GetComponent<Renderer>().materials.Length; i++)
+                    {
+                        child.gameObject.GetComponent<Renderer>().materials[i].DisableKeyword("_EMISSION");
+                    }
+            }
+        }
+        //}
+
+    }
+
+
+    public override void Interact(GameObject interacter)
+    {
+        if (((collectable == true && collect == true) && !dialogue) || (collectable == false && !dialogue))
+        {
+            dialoguetrigger.TriggerDialogue();
+            //do dialogue
+        }
+        if (collectable == true && collect == false)
+        {
+            collect = true;
+            GetComponent<SC_NPCFollow>().enabled = true;
+        }
+        TurnOff();
+    }
+
+    public override bool GetAnimatable()
+    {
+        return animatable;
+    }
+    public override bool GetCollectable()
+    {
+        return collectable;
+    }
+    public override bool GetInteract()
+    {
+        return interact;
+    }
+
+    public override bool GetCollect()
+    {
+        return collect;
+    }
+    public override void SetAnimatable(bool newvalue)
+    {
+        animatable = newvalue;
+    }
+    public override void SetCollectable(bool newvalue)
+    {
+        collectable = newvalue;
+    }
+    public override void SetInteract(bool newvalue)
+    {
+        interact = newvalue;
+    }
+    public override void SetCollect(bool newvalue)
+    {
+        collect = newvalue;
+    }
+
+}
+
+
