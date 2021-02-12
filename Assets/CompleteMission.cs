b27e751e124fff3b209aef254fd1bbc82c0ea614
silviaraggi_ <silvia.raggi.95@gmail.com>
2021-02-12 21:14:50 +0100

    Animazione_Missione_compiuta

---
+++ Assets/CompleteMission.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteMission : MonoBehaviour
+{
+    public GameObject missionCompleteUI;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Time.timeScale = 0.5f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale; //SlowmotionEffect
+            Debug.Log("Missione Complete");
+            missionCompleteUI.SetActive(true);
+            
+        }
+    }
+}
+
+    
+
+    
