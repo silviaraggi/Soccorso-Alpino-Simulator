ed16eebcc75274a528037b1b2cd7dc6b07ef21a2
silviaraggi_ <silvia.raggi.95@gmail.com>
2021-02-13 23:17:11 +0100

    Mappa GPS + script SCENA BAITA (da inserire dopo cutscene)

---
+++ Assets/Mappa_On.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mappa_On : MonoBehaviour
+{
+    public GameObject mappa_on;
+    // Update is called once per frame
+    void Update()
+    {
+        mappa_on.SetActive(true);
+
+    }
+}
