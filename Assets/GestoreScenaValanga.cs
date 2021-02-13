c1cc279113a13cb3441f65f361015a1f550895fd
AndreaBona <andrea.bona1996@gmail.com>
2021-02-13 13:20:10 +0100

    Scena Valanga prime modifiche

---
+++ Assets/GestoreScenaValanga.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Playables;
+public class GestoreScenaValanga : MonoBehaviour
+{
+    private GameObject Soccorritore1;
+    private GameObject Soccorritore2;
+    private GameObject Disperso;
+    private GameObject Timeline;
+    public PlayableDirector director;
+    private GameObject player;
+    public bool play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play = false;
+        player = GameObject.Find("Player");
+        Timeline = GameObject.Find("Timeline");
+        Disperso = GameObject.Find("Disperso");
+        Soccorritore1 = GameObject.Find("SoccorritoreNeve1");
+        Soccorritore2 = GameObject.Find("SoccorritoreNeve2");
+        director = Timeline.GetComponent<PlayableDirector>();
+        foreach (Renderer daAttivare in Soccorritore1.GetComponentsInChildren<Renderer>())
+        {
+            daAttivare.enabled = true;
+        }
+        Soccorritore2.transform.position = new Vector3(72.22f, 36.43f, 134.61f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (director.state != PlayState.Playing)
+            play = true;
+        if (play)
+        {
+
+            foreach (Renderer daAttivare in Soccorritore1.GetComponentsInChildren<Renderer>())
+            {
+                daAttivare.enabled = false;
+            }
+            GameObject.Find("MainCamera").GetComponent<Camera>().enabled = true;
+            Soccorritore2.GetComponent<SC_NPCFollow>().enabled = true;
+            Soccorritore2.GetComponent<NavMeshAgent>().enabled = true;
+            Soccorritore2.GetComponent<soccorritoreNeve2>().enabled = true;
+            //do stuff
+        }
+
+    }
+}
---
+++ Assets/soccorritoreNeve2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class soccorritoreNeve2 : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform ToFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GameObject.Find("SoccorritoreNeve2").GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToFollow = GameObject.Find("Player").transform;
+        UpdatePosition();
+    }
+
+    void UpdatePosition() {
+        agent.destination = ToFollow.transform.position;
+        Vector3 GoHere = ToFollow.transform.position;
+        Vector3 npcPos = gameObject.transform.position;
+        Vector3 delta = new Vector3(GoHere.x - npcPos.x, 0.0f, GoHere.z - npcPos.z);
+        Quaternion rotation = Quaternion.LookRotation(delta);
+        gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, rotation, 0.5f);
+        if (agent.velocity.magnitude < 0.15f)
+        {
+            GetComponent<Animator>().SetBool("isWalking", false);
+            GetComponent<Animator>().SetBool("isIdle", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isWalking", true);
+            GetComponent<Animator>().SetBool("isIdle", false);
+        }
+    }
+
+}
