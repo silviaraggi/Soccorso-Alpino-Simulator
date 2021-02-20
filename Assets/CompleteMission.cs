429a394a18e1475710a329296cfe69aba9309961
DESKTOP-58HGMR5\Rosario1 <milazzorosario@hotmail.it>
2021-02-20 17:45:53 +0100

    Sistemato missione completata

--- Assets/CompleteMission.cs
+++ Assets/CompleteMission.cs
@@ -9,16 +9,7 @@ public class CompleteMission : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            Time.fixedDeltaTime = 0.02f * Time.timeScale; //SlowmotionEffect
-            Debug.Log("Missione Complete");
-            missionCompleteUI.SetActive(true);
-            
-        }
-    }
+
      public void Fine()
     {
         Time.fixedDeltaTime = 0.02f * Time.timeScale; //SlowmotionEffect
